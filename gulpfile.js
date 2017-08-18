72e11d82f339baf96325280c94ffd2d7db80ed86
varHarrie <varharrie@gmail.com>
2017-08-18 16:52:17 +0800

    Publish
     - circe-body-parser@0.0.1
     - circe-checker@0.0.1
     - circe-config@0.0.1
     - circe-cors@0.0.1
     - circe-jwt@0.0.1
     - circe-on-error@0.0.1
     - circe-response@0.0.1
     - circe-unless@0.0.1

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp')
+const path = require('path')
+const del = require('del')
+const ts = require('gulp-typescript')
+
+const root = __dirname
+
+const packages = [
+  // 'circe-body-parser',
+  // 'circe-checker',
+  // 'circe-config',
+  // 'circe-cors',
+  // 'circe-jwt',
+  // 'circe-on-error',
+  // 'circe-response',
+  'circe-unless'
+]
+
+function getPackageRoot (name) {
+  return path.resolve(root, 'packages', name)
+}
+
+packages.forEach((package) => {
+  gulp.task(`build:${package}`, function () {
+    const packageRoot = getPackageRoot(package)
+    const project = ts.createProject(path.join(packageRoot, 'tsconfig.json'))
+    return gulp.src(`${packageRoot}/src/**/*.ts`)
+      .pipe(project())
+      .pipe(gulp.dest(path.join(packageRoot, 'dist')))
+  })
+})
+
+gulp.task('clean', function() {
+    return del(['packages/*/dist'])
+})
+
+gulp.task('build', packages.map((package) => `build:${package}`))
