ff6190f0e1a16a95ef661eff904212274e99d1ae
varHarrie <varharrie@gmail.com>
2017-08-21 10:45:25 +0800

    update: add circe-combine-routes

---
+++ packages/circe-combine-routers/src/index.ts
@@ -0,0 +1,18 @@
+import * as Koa from 'koa'
+import * as Router from 'koa-router'
+import * as compose from 'koa-compose'
+
+function combineRoutes (routers: Router[], mounted?: string): Koa.Middleware {
+  const middlewares: Koa.Middleware[] = []
+
+  routers.forEach((router) => {
+    if (mounted) {
+      router.prefix(mounted)
+    }
+
+    middlewares.push(router.routes())
+    middlewares.push(router.allowedMethods())
+  })
+
+  return compose(middlewares)
+}
