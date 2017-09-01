2eaae0a1452872136cfa6b8678b29a27862998b1
varHarrie <varharrie@gmail.com>
2017-09-01 17:40:56 +0800

    fix: variable name

--- packages/circe-combine-routers/src/index.ts
+++ packages/circe-combine-routers/src/index.ts
@@ -2,12 +2,12 @@ import * as Koa from 'koa'
 import * as Router from 'koa-router'
 import * as compose from 'koa-compose'
 
-export default function combineRoutes (routers: Router[], mounted?: string): Koa.Middleware {
+export default function combineRoutes (routers: Router[], mount?: string): Koa.Middleware {
   const middlewares: Koa.Middleware[] = []
 
   routers.forEach((router) => {
-    if (mounted) {
-      router.prefix(mounted)
+    if (mount) {
+      router.prefix(mount)
     }
 
     middlewares.push(router.routes())
